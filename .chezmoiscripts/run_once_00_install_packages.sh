80d2475a2f4c24cc03652cb18d02859c5e5c0425
Talha Yildirim <talha.yildirim720@gmail.com>
2025-08-29 23:25:30 -0400

    improving install script

--- .chezmoiscripts/run_once_00_install_packages.sh
+++ .chezmoiscripts/run_once_00_install_packages.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-if [ -f "/etc/arch-release" ]; then
-  echo -e "Arch-based system detected, proceeding with script. \n"
+# Source os-release to check for cachyos
+source /etc/os-release
+
+if [[ "$ID" == "cachyos" ]]; then
+  echo -e "Cachy system detected, proceeding with script. \n"
+elif [ -f "/etc/arch-release" ]; then
+
+  echo -e "Arch-based system detected, adding Cachy repos before proceeding with script. \n"
+
+  # Import the repository key
+  sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+  # Sign the repository key
+  sudo pacman-key --lsign-key F3B607488DB35A47
+  # Install the necessary packages
+  sudo pacman -U 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' \
+    'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
+
+  # Add the CachyOS repositories to the pacman config file
+  cat $HOME/.local/share/chezmoi/.chezmoiscripts >>/etc/pacman.conf
+
+  # Update your system with CachyOS packages
+  sudo pacman -Syu
+
 else
   echo "Script only works for Arch-based distributions."
   echo "Aborting..."
@@ -12,13 +36,19 @@ echo "Installing packages"
 
 # Putting everything on a new line for readablity
 sudo pacman -S cachyos-extra-v3/git
-sudo pacman -S cachyos/paru
 sudo pacman -S cachyos/zen-browser-bin
 sudo pacman -S cachyos-extra-v3/spotify-launcher
 sudo pacman -S cachyos-extra-v3/alacritty
 sudo pacman -S cachyos-extra-v3/btop
 sudo pacman -S cachyos-extra-v3/fastfetch
 sudo pacman -S extra/discord
+sudo pacman -S extra/signal-desktop
+
+echo -e "Installing Paru now. Process takes some extra steps \n"
+
+sudo pacman -S cachyos/paru
+
+mkdir ~/.config/paru
 
 echo -e "Installing Vim now. Process takes some extra steps \n"
 
